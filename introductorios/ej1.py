ab27d4f7db64cc1406068af140db4c47aabc0e29
Bruno Panizza <brunocapopanizza@hotmail.com>
2026-03-23 19:29:54 -0300

    inicio practica 1

---
+++ introductorios/ej1.py
@@ -0,0 +1,5 @@
+anio = int(input("Ingrese su fecha de nacimiento:"))
+
+print ("Cumplirá 18 en el año: ", anio+18)
+print ("Cumplirá 21 en el año: ", anio+21)
+print ("Cumplirá 100 en el año: ", anio+100)
