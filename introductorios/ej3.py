c1e847c33714214fcd10bcab3ff1bdaa5526f029
Bruno Panizza <brunocapopanizza@hotmail.com>
2026-03-23 22:57:17 -0300

    ejercicios hasta el 4

---
+++ introductorios/ej3.py
@@ -0,0 +1,7 @@
+#Crea un programa que solicite al usuario un número y muestre su tabla de multiplicar
+#del 1 al 10 utilizando un bucle for.
+
+num = int(input('Ingrese un numero: '))
+ 
+for i in range(1,11):
+    print(num*i)
