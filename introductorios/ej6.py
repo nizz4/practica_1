3ea144f78748556a95ff037a62ed8b3fd2fe5903
Bruno Panizza <brunocapopanizza@hotmail.com>
2026-03-24 18:08:07 -0300

    ej6 limpio

--- introductorios/ej6.py
+++ introductorios/ej6.py
@@ -1,5 +1,3 @@
-#dado un número N ingresado por el usuario, imprima los números del 1 al N pero saltee los múltiplos de 5. Nota: utilizá la sentencia continuedonde haga falta.
-
 n = int(input("Ingrese un numero: "))
 
 multiplos_5 = [i for i in range(1,n+1) if (i%5 == 0)]
