1ce25e4ad07f9c07b71d9c69a4b12d6adcec5c97
Bruno Panizza <brunocapopanizza@hotmail.com>
2026-03-24 17:27:42 -0300

    ejercicio 4 completo

--- introductorios/ej3.py
+++ introductorios/ej3.py
@@ -1,6 +1,3 @@
-#Crea un programa que solicite al usuario un número y muestre su tabla de multiplicar
-#del 1 al 10 utilizando un bucle for.
-
 num = int(input('Ingrese un numero: '))
  
 for i in range(1,11):
---
+++ introductorios/ej4.py
@@ -0,0 +1,9 @@
+num = int(input('Ingrese un numero: '))
+
+for i in range (1,num+1):
+    if ((i%5)!=0):
+        print(i)
+
+
+multiplos_no_5 = [i for i in range(1,num+1) if i%5 != 0]
+print(multiplos_no_5)
