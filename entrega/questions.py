9bb862723fa4df76675ede6c48cde74985b45d68
Bruno Panizza <brunocapopanizza@hotmail.com>
2026-03-24 22:27:44 -0300

    Corrección del bug de entrada de datos

--- entrega/questions.py
+++ entrega/questions.py
@@ -32,15 +32,18 @@ while attempts > 0:
      print(f"Intentos restantes: {attempts}")
      print(f"Letras usadas: {', '.join(guessed)}")
      letter = input("Ingresá una letra: ")
-     if letter in guessed:
-        print("Ya usaste esa letra.")
-     elif letter in word:
-         guessed.append(letter)
-         print("¡Bien! Esa letra está en la palabra.")
+     if letter.isalpha() and len(letter)==1:
+         if letter in guessed:
+            print("Ya usaste esa letra.")
+         elif letter in word:
+             guessed.append(letter)
+             print("¡Bien! Esa letra está en la palabra.")
+         else:
+             guessed.append(letter)
+             attempts -= 1
+             print("Esa letra no está en la palabra.")
      else:
-         guessed.append(letter)
-         attempts -= 1
-         print("Esa letra no está en la palabra.")
+         print("Entrada no válida")
      print()
 else:
     print(f"¡Perdiste! La palabra era: {word}")
